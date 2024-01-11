6a06ae1b157667abfb916016aac510636afc1c65
Nazrul7363 <nazrulansari8011@gmail.com>
2024-01-11 14:50:16 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+
+import DrawerComponents from "./components/DrawerComponents";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#2490ef",
+    secondary: "#2490ef",
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/DrawerComponents.js
@@ -0,0 +1,15 @@
+import { Drawer } from "react-native-paper";
+import React from "react";
+
+export default function DrawerComponents(theme) {
+  return (
+    <Drawer.Section title="Some title">
+      <Drawer.Item
+      label="first"
+      style={theme}
+      
+      >jhjgkjhk</Drawer.Item>
+
+    </Drawer.Section>
+  );
+}
